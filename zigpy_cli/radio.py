26871bad7b656ba8453368f3b0457fda80e19549
puddly <32534428+puddly@users.noreply.github.com>
2022-10-09 15:14:57 -0400

    Add network `reset`, `permit`, and `change-channel` commands (#20)
    * Add a `change_channel` command
    
    * Add a `reset` command
    
    * Add a `permit` command
    
    * Add documentation
    
    * Only send a single channel change broadcast

--- zigpy_cli/radio.py
+++ zigpy_cli/radio.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import json
+import asyncio
 import logging
 import importlib
 import itertools
@@ -8,6 +9,7 @@ import collections
 import importlib.util
 
 import click
+import zigpy.zdo
 import zigpy.state
 import zigpy.types
 import zigpy.zdo.types
@@ -144,6 +146,24 @@ async def form(app):
 
 @radio.command()
 @click.pass_obj
+@click_coroutine
+async def reset(app):
+    await app.connect()
+    await app.reset_network_info()
+
+
+@radio.command()
+@click.pass_obj
+@click.option("-t", "--join-time", type=int, default=250)
+@click_coroutine
+async def permit(app, join_time):
+    await app.startup(auto_form=True)
+    await app.permit(join_time)
+    await asyncio.sleep(join_time)
+
+
+@radio.command()
+@click.pass_obj
 @click.option("--nwk", type=HEX_OR_DEC_INT, default=0x0000)
 @click.option("-n", "--num-scans", type=int, default=-1)
 @click_coroutine
@@ -209,3 +229,32 @@ async def energy_scan(app, nwk, num_scans):
             )
 
         print()
+
+
+@radio.command()
+@click.pass_obj
+@click.option("-c", "--channel", type=int)
+@click_coroutine
+async def change_channel(app, channel):
+    await app.startup()
+
+    await zigpy.zdo.broadcast(
+        app=app,
+        command=zigpy.zdo.types.ZDOCmd.Mgmt_NWK_Update_req,
+        grpid=None,
+        radius=0,
+        broadcast_address=zigpy.types.BroadcastAddress.ALL_DEVICES,
+        NwkUpdate=zigpy.zdo.types.NwkUpdate(
+            ScanChannels=zigpy.types.Channels.from_channel_list([channel]),
+            ScanDuration=zigpy.zdo.types.NwkUpdate.CHANNEL_CHANGE_REQ,
+            nwkUpdateId=app.state.network_info.nwk_update_id + 1,
+        ),
+    )
+
+    await app.get_device(nwk=0x0000).zdo.Mgmt_NWK_Update_req(
+        zigpy.zdo.types.NwkUpdate(
+            ScanChannels=zigpy.types.Channels.from_channel_list([channel]),
+            ScanDuration=zigpy.zdo.types.NwkUpdate.CHANNEL_CHANGE_REQ,
+            nwkUpdateId=app.state.network_info.nwk_update_id + 1,
+        ),
+    )
