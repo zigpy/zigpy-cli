7d784436a34ef49730b4d667e7e705bfdc746c2b
puddly <32534428+puddly@users.noreply.github.com>
2023-02-14 12:00:25 -0500

    Create the `ota generate-index` command

--- zigpy_cli/ota.py
+++ zigpy_cli/ota.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import hashlib
+import json
 import logging
 import pathlib
 
@@ -65,3 +67,61 @@ def dump_firmware(input, output):
                 break
         else:
             LOGGER.warning("Image has no UPGRADE_IMAGE subelements")
+
+
+@ota.command()
+@click.pass_context
+@click.option("--ota-url-root", type=str, default=None)
+@click.option("--output", type=click.File("w"), default="-")
+@click.argument("files", nargs=-1, type=pathlib.Path)
+def generate_index(ctx, ota_url_root, output, files):
+    if ctx.parent.parent.params["verbose"] == 0:
+        cli.callback(verbose=1)
+
+    ota_metadata = []
+
+    for f in files:
+        if not f.is_file():
+            continue
+
+        LOGGER.info("Parsing %s", f)
+        contents = f.read_bytes()
+
+        try:
+            image, rest = parse_ota_image(contents)
+        except Exception as e:
+            LOGGER.error("Failed to parse: %s", e)
+            continue
+
+        if rest:
+            LOGGER.error("Image has trailing data: %r", rest)
+            continue
+
+        try:
+            validate_ota_image(image)
+        except Exception as e:
+            LOGGER.error("Image is invalid: %s", e)
+
+        if ota_url_root is not None:
+            url = f"{ota_url_root.rstrip('/')}/{f.name}"
+        else:
+            url = None
+
+        metadata = {
+            "binary_url": url,
+            "file_version": image.header.file_version,
+            "image_type": image.header.image_type,
+            "manufacturer_id": image.header.manufacturer_id,
+            "changelog": "",
+            "checksum": f"sha3-256:{hashlib.sha3_256(contents).hexdigest()}",
+        }
+
+        if image.header.hardware_versions_present:
+            metadata["min_hardware_version"] = image.header.minimum_hardware_version
+            metadata["max_hardware_version"] = image.header.maximum_hardware_version
+
+        LOGGER.info("Writing %s", f)
+        ota_metadata.append(metadata)
+
+    json.dump(ota_metadata, output, indent=4)
+    output.write("\n")
